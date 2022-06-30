a7a77179dbd0e7bc55c33ce5b84d84d38bbef0d1
oyindamolaWbCode <kushimo.oyindamola@gmail.com>
2022-06-30 19:45:41 +0100

    [NEW] added function for toggle

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //Grab the DOM
-const toggler = document.getElementsByClassName("toggler");
+const toggler = document.querySelector(".toggler");
 const todoCheck = document.getElementsByClassName("todo-check");
 const todoText = document.getElementsByClassName("todo-text");
 const time = document.getElementById("time");
@@ -15,6 +15,7 @@ const submit = document.querySelector('#submit');
 const container = document.querySelector('.container-mylist');
 const del = document.querySelector('.delete');
 const body =document.querySelector('body');
+const card = document.querySelector('.card');
 
 
 //alert message when inputed not filled
@@ -55,4 +56,38 @@ function result(){
     }
 }
 
+//mark/check the todo when completed
+function containerList(){
+  container.delete()
+  console.log("deleted");
+}
+
+//change of theme
+function togglerTheme(){
+
+toggleLight();
+
+}
+
+
+function toggleDark(){
+  body.style.background = 'var(--very-dark-blue)';
+  body.style.backgroundImage = 'url(./images/bg-desktop-dark.jpg)';
+  body.style.backgroundRepeat = 'no-repeat';
+  toggler.src="./images/icon-sun.svg";
+  card.style.backgroundColor='var(--very-dark-desaturated-blue)';
+   document.querySelector('.toggler').addEventListener('click',toggleLight)
+ 
+  }
+
+function toggleLight(){
+  body.style.color ='white';
+  body.style.backgroundImage="url('./images/bg-desktop-light.jpg')";
+  body.style.backgroundRepeat = 'no-repeat';
+  body.style.color = 'var(--very-dark-grayish-blue)';
+  toggler.src="./images/icon-moon.svg";
+  card.style.backgroundColor='hsl(233, 11%, 84%)';
+  container.style.backgroundColor='hsl(233, 11%, 84%)';
+  document.querySelector('.toggler').addEventListener('click',toggleDark)
+}
 
